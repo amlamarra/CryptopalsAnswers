f5e2f9048d9033c773ab4c28bdbf843521b7561b
amlamarra <andrew.lamarra@gmail.com>
2016-10-11 15:04:07 -0400

    Create hex_to_str.py
    While this isn't the solution to one of the challenges, I'll be using this code in other answers.

---
+++ set1/hex_to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# Takes a string (even number) of hex characters and returns a string
+def hex_to_str(in_str):
+	return "".join([chr(int(in_str[i*2:i*2+2],16)) 
+			for i in range(int(len(in_str)/2))])
+
+if __name__ == "__main__":
+	import argparse
+	
+	parser = argparse.ArgumentParser(
+		description="Converts hex characters to an ascii string")
+	parser.add_argument("string", nargs="?", help="The hex string")
+	args = parser.parse_args()
+	if args.string:
+		instr = args.string
+	else:
+		instr = input("Enter the hex: ")
+	
+	print(hex_to_str(instr))
