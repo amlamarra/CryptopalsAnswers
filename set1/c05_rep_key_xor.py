4feb212fdef8eb47932b5438e53534fa2fbc2426
amlamarra <andrew.lamarra@gmail.com>
2016-10-27 13:59:18 -0400

    Update c05_rep_key_xor.py

--- set1/c05_rep_key_xor.py
+++ set1/c05_rep_key_xor.py
@@ -1,41 +1,53 @@
 #!/usr/bin/python3
-from c02_str_xor import *
-from str_to_hex import *
 import argparse
+from c02_str_xor import *
+from conversions import *
 
-def str_xor_key(instr, key):
-	strlen = len(instr)
-	keylen = len(key)
-	
-	# Make the string length evenly divisible by the key length
-	if strlen % keylen:
-		instr += "a" * (keylen - (strlen % keylen))
-	
-	outstr = "".join([str_xor(str_to_hex(instr[i*keylen:i*keylen+keylen]), str_to_hex(key)) 
-					  for i in range(int(len(instr)/keylen))])
-	
-	# Here's the same thing, but more readable
-	#outstr = ""
-	#for i in range(int(len(instr)/keylen)):
-	#	outstr += str_xor(str_to_hex(instr[i*keylen:i*keylen+keylen]), str_to_hex(key))
-	
-	return outstr[:strlen*2]
-	
+
+def str_xor_key(msg, key):
+    """ XOR a string with a repeating key
+    ACCEPTS: Either a string or bytes for both the message & key
+    RETURNS: a hex string
+    """
+    
+    msg = str_to_hex(msg)
+    key = str_to_hex(key)
+    
+    mlen = len(msg)
+    klen = len(key)
+    
+    # Make the string length evenly divisible by the key length
+    if mlen % klen:
+        msg += "0" * (klen - (mlen % klen))
+    
+    out_str = "".join([str_xor(msg[i:i+klen], key)
+                      for i in range(0, len(msg), klen)])
+    
+    return out_str[:mlen]
+
+
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description="Repeating-key XOR with a longer string \
-									 (make sure the key is shorter)")
-	parser.add_argument("string", nargs="?", help="The string in ASCII")
-	parser.add_argument("key", nargs="?", help="The key to XOR with the string")
-	args = parser.parse_args()
-	
-	if args.string:
-		STRING = args.string
-	else:
-		STRING = "Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal"
-	
-	if args.key:
-		KEY = args.key
-	else:
-		KEY = "ICE"
-	
-	print(str_xor_key(STRING, KEY))
+    
+    parser = argparse.ArgumentParser(description="Repeating-key XOR with a " \
+                                     "longer string (the key is shorter)")
+    parser.add_argument("string", nargs="?", help="The string in ASCII")
+    parser.add_argument("key", nargs="?", help="The key to XOR with the string")
+    args = parser.parse_args()
+    
+    if args.string:
+        STRING = args.string
+    else:
+        STRING = "Burning 'em, if you ain't quick and " \
+                 "nimble I go crazy when I hear a cymbal"
+    
+    if args.key:
+        KEY = args.key
+    else:
+        KEY = "ICE"
+    
+    print("Original message:")
+    print(STRING)
+    print("The key: {}".format(KEY))
+    
+    print("\nMessage encrypted with key:")
+    print(str_xor_key(STRING, KEY))
