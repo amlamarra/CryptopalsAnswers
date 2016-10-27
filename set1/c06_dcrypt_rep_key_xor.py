3f2a620423c76f09393a1b99188c3a5d4a87a6ba
amlamarra <andrew.lamarra@gmail.com>
2016-10-27 14:01:21 -0400

    Update c06_dcrypt_rep_key_xor.py

--- set1/c06_dcrypt_rep_key_xor.py
+++ set1/c06_dcrypt_rep_key_xor.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-from str_to_hex import *
+import base64
+from conversions import *
 
-# Calculates the hamming distance of two strings
+
 def ham_dist(str1, str2):
-	xord = int(str_to_hex(str1),16) ^ int(str_to_hex(str2),16)
-	
-	count = 0
-	while xord > 0:
-		if xord % 2:
-			count += 1
-		xord >>= 1
-	
-	return count
+    """ Calculates the hamming distance of two strings
+    The Hamming distance is just the number of differing bits
+    ACCEPTS: Two strings of equal length
+    RETURNS: An integer of the Edit/Hamming Distance
+    """
+    
+    xord = int(str_to_hex(str1),16) ^ int(str_to_hex(str2),16)
+    
+    count = 0
+    while xord > 0:
+        if xord % 2:
+            count += 1
+        xord >>= 1
+    
+    return count
+
 
 if __name__ == "__main__":
-	print(ham_dist("this is a test", "wokka wokka!!!"))
+    print(ham_dist("this is a test", "wokka wokka!!!"))
