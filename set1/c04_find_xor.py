d396612dc0636b605ab312bd346f19572643fa17
amlamarra <andrew.lamarra@gmail.com>
2016-10-28 21:51:45 -0400

    Update c04_find_xor.py

--- set1/c04_find_xor.py
+++ set1/c04_find_xor.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from c03_dcrypt_xor import *    # str_xor_c, dcrypt_xor, freq_score
-from hex_to_str import hex_to_str
+from conversions import *
 
 # This will use the list of hex strings in 04.txt and find the one that has
 # been XOR'ed with a single byte.
 strings = [line.strip() for line in open("04.txt")]
-decrypted = [dcrypt_xor(string) for string in strings]
+decrypted = [dcrypt_xor(hex_to_byte(string)) for string in strings]
 scores = [freq_score(i[1]) for i in decrypted]
 print(decrypted[scores.index(max(scores))][1])
