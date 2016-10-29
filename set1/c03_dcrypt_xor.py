ed9459a3140e843df222b59a3454c9aa43e43c2c
amlamarra <andrew.lamarra@gmail.com>
2016-10-28 21:51:22 -0400

    Update c03_dcrypt_xor.py

--- set1/c03_dcrypt_xor.py
+++ set1/c03_dcrypt_xor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from conversions import hex_to_str
+from conversions import *
 import argparse
+from Crypto.Util.strxor import strxor_c
 
 
 def str_xor_c(instr, char):
@@ -9,7 +10,7 @@ def str_xor_c(instr, char):
     RETURNS: Ascii string XOR'ed with the character
     """
     
-    return "".join([chr(ord(instr[c])^ord(char)) for c in range(len(instr))])
+    return "".join([chr(instr[c] ^ ord(char)) for c in range(len(instr))])
 
 
 def freq_score(instr):
@@ -19,53 +20,53 @@ def freq_score(instr):
     """
     
     freqs = { # www.data-compression.com/english.html
-        'a': 651738,
-        'b': 124248,
-        'c': 217339,
-        'd': 349835,
-        'e': 1041442,
-        'f': 197881,
-        'g': 158610,
-        'h': 492888,
-        'i': 558094,
-        'j': 9033,
-        'k': 50529,
-        'l': 331490,
-        'm': 202124,
-        'n': 564513,
-        'o': 596302,
-        'p': 137645,
-        'q': 8606,
-        'r': 497563,
-        's': 515760,
-        't': 729357,
-        'u': 225134,
-        'v': 82903,
-        'w': 171272,
-        'x': 13692,
-        'y': 145984,
-        'z': 7836,
-        ' ': 1918182
+        'a': 0.0651738,
+        'b': 0.0124248,
+        'c': 0.0217339,
+        'd': 0.0349835,
+        'e': 0.1041442,
+        'f': 0.0197881,
+        'g': 0.0158610,
+        'h': 0.0492888,
+        'i': 0.0558094,
+        'j': 0.0009033,
+        'k': 0.0050529,
+        'l': 0.0331490,
+        'm': 0.0202124,
+        'n': 0.0564513,
+        'o': 0.0596302,
+        'p': 0.0137645,
+        'q': 0.0008606,
+        'r': 0.0497563,
+        's': 0.0515760,
+        't': 0.0729357,
+        'u': 0.0225134,
+        'v': 0.0082903,
+        'w': 0.0171272,
+        'x': 0.0013692,
+        'y': 0.0145984,
+        'z': 0.0007836,
+        ' ': 0.1918182
     }
     
     score = 0
-    for c in instr:
-            if c.lower() in freqs:
-                score += freqs[c.lower()]
+    for c in instr.lower():
+            if c in freqs:
+                score += freqs[c]
     return score
 
 
-def dcrypt_xor(xor_str):
+def dcrypt_xor(bytes):
     """ Find the key a string was XOR'ed with
-    ACCEPTS: String of XOR'ed characters in hex
+    ACCEPTS: String of byte characters
     RETURNS: A tuple of the key and the decrypted string
     The key is the decimal value of the character the string was XOR'ed with
     """
     
-    string = hex_to_str(xor_str)
-    scores = [freq_score(str_xor_c(string, chr(i))) for i in range(256)]
+    scores = [freq_score(str_xor_c(bytes, chr(i))) for i in range(256)]
     key = scores.index(max(scores))
-    return (key, str_xor_c(string, chr(key)))
+    
+    return (key, str_xor_c(bytes, chr(key)))
 
 
 if __name__ == "__main__":
@@ -79,7 +80,7 @@ if __name__ == "__main__":
         code = args.hex
     else:
         code = "1b37373331363f78151b7f2b783431333d" \
-			   "78397828372d363c78373e783a393b3736"
-    answer = dcrypt_xor(code)
+               "78397828372d363c78373e783a393b3736"
+    answer = dcrypt_xor(hex_to_byte(code))
     print("Key = {}".format(answer[0]))
-    print("String = {}".format(answer[1].encode()))
+    print("String = {}".format(answer[1]))
