75e0d54037ce5546edc648652d0cdc31983695cf
amlamarra <andrew.lamarra@gmail.com>
2016-11-11 12:39:01 -0500

    Update c07_aes_128_ecb.py

--- set1/c07_aes_128_ecb.py
+++ set1/c07_aes_128_ecb.py
@@ -7,10 +7,18 @@ from Crypto.Cipher import AES
 import base64
 
 def encrypt(message, passphrase):
+    """ Encrypts a message in AES ECB mode with a given key
+    ACCEPTS: Two strings, the plaintext message and the key
+    RETURNS: A bytes string of base64 encoded ciphertext
+    """
     aes = AES.new(passphrase, AES.MODE_ECB)
     return base64.b64encode(aes.encrypt(message))
 
 def decrypt(encrypted, passphrase):
+    """ Decrypts a ciphertext in AES ECB mode with a given key
+    ACCEPTS: Two strings, the ciphertext and the key
+    RETURNS: A bytes string of the plaintext message
+    """
     aes = AES.new(passphrase, AES.MODE_ECB)
     return aes.decrypt(base64.b64decode(encrypted))
 
